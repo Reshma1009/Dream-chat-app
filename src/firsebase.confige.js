2bb92f3233f224eb8c2714181a0acaa9e24d6d14
ProgramerNila <programernila@gmail.com>
2023-03-21 03:35:37 +0600

    firebase add

---
+++ src/firsebase.confige.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYycMnXygjeJelSs723EvECG6WUUCkn_8",
+  authDomain: "dream-chat-app-7a9a1.firebaseapp.com",
+  projectId: "dream-chat-app-7a9a1",
+  storageBucket: "dream-chat-app-7a9a1.appspot.com",
+  messagingSenderId: "632103302719",
+  appId: "1:632103302719:web:7265870daa161eded26a8a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
