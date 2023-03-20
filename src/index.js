e895fc492d32ba4c5fe62dfa2fea1728d8f574c5
ProgramerNila <programernila@gmail.com>
2023-03-21 03:41:50 +0600

    add redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import firebaseConfig from './firsebase.confige';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import firebaseConfig from "./firsebase.confige";
+import store from "./store";
+import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/slices/userSlices.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    userInfo: null,
+  },
+  reducers: {
+    usersInformation: (state, action) => {
+      state.userInfo = action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { usersInformation } = userSlice.actions;
+
+export default userSlice.reducer;
